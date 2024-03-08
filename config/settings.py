bcd2ef33b23e74cdfcaa0a3327f1a71aabc1f5bc
Abdumalik Abdusalomov <aabdusalomov533@gmail.com>
2024-03-08 11:19:53 +0500

    adding build.sh

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgres://abdumalik:24JSgixY0RnhcQAF0vNAV2MraPfhF9BP@dpg-cnlaod821fec73c4j6l0-a/books_yzd5',
         conn_max_age=600
     )
 }
