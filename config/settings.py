0e978676cead6fdfcc03a5fa8d2066eab4069494
Abdumalik Abdusalomov <aabdusalomov533@gmail.com>
2024-03-08 11:14:27 +0500

    adding build.sh

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -80,14 +82,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "Books",
-        'USER': "postgres",
-        'PASSWORD': "1912",
-        'HOST': "localhost",
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
