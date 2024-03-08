489c847eb94e429d3a3b4409485036ea0ebfa69f
Abdumalik Abdusalomov <aabdusalomov533@gmail.com>
2024-03-08 11:35:48 +0500

    DEBUG false

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8qjj2re&5-66g5jg91^ew5f(0+u_o0^%bs&ht-^jrpsw+u94-7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", ".onrender.com"]
 
